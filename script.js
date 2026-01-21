f499d04b63cab583b5292738e41736b7c11afc87
tuanminhongthanlan <tuannminhh496@gmail.com>
2026-01-21 21:08:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-function createHeart() {
-    const heart = document.createElement("div");
-    heart.className = "heart";
-    heart.innerText = "❤️";
-
-    heart.style.left = Math.random() * 100 + "vw";
-
-    document.body.appendChild(heart);
-
-    setTimeout(() => {
-        heart.remove();
-    }, 4000);
+function yes() {
+    alert("da iu em nhat");
 }
 
-setInterval(createHeart, 300);
+const noBtn = document.getElementById("no");
+noBtn.onmouseover = function () {
+    noBtn.style.position = "absolute";
+    noBtn.style.left = Math.random() * window.innerWidth + "px";
+    noBtn.style.top = Math.random() * window.innerHeight + "px";
+};
