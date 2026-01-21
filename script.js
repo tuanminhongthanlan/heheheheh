bfe698da0c8d42347749c4f203c033e8d087315c
tuanminhongthanlan <tuannminhh496@gmail.com>
2026-01-21 21:04:52 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.className = "heart";
+    heart.innerText = "❤️";
+
+    heart.style.left = Math.random() * 100 + "vw";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 4000);
+}
+
+setInterval(createHeart, 300);
